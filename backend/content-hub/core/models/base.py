79a0719f475d558d46d33478480c2f4d7ba080bd
Artem <progerguru@gmail.com>
2025-03-05 00:47:32 +0300

    feat(models): add Base class for SQLAlchemy models with automatic table naming
    - Added `Base` class inheriting from `DeclarativeBase` as the base class for all database models.
    - Implemented automatic table name generation using `__tablename__` directive.
    - Added `id` field as a primary key for all models.

---
+++ backend/content-hub/core/models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+from utils import camel_to_snake
+
+
+class Base(DeclarativeBase):
+	"""Base class for all database models"""
+
+	__abstract__ = True  # This class will not create a table in the database
+
+	@declared_attr.directive
+	def __tablename__(cls) -> str:  # Auto-generation of the table name based on the class name
+		return camel_to_snake(cls.__name__) + "s"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
