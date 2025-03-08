76e5b2bb8ccdfb17fbf616577087be94c2697558
Artem <progerguru@gmail.com>
2025-03-08 12:35:37 +0300

    feat(models): set metadata naming convention

--- backend/content-hub/core/models/base.py
+++ backend/content-hub/core/models/base.py
@@ -1,5 +1,7 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
+from core.config import settings
 from utils import camel_to_snake
 
 
@@ -7,6 +9,10 @@ class Base(DeclarativeBase):
 	"""Base class for all database models"""
 
 	__abstract__ = True  # This class will not create a table in the database
+
+	metadata = MetaData(
+		naming_convention=settings.db.naming_convention,
+	)
 
 	@declared_attr.directive
 	def __tablename__(cls) -> str:  # Auto-generation of the table name based on the class name
