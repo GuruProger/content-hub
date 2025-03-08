bb24704e91ecd4a2a025f1dd00831a817dadbb76
Artem <progerguru@gmail.com>
2025-03-08 17:39:06 +0300

    refactor(models): remove duplicate code in Base class

--- backend/content-hub/core/models/base.py
+++ backend/content-hub/core/models/base.py
@@ -17,5 +17,3 @@ class Base(DeclarativeBase):
 	@declared_attr.directive
 	def __tablename__(cls) -> str:  # Auto-generation of the table name based on the class name
 		return camel_to_snake(cls.__name__) + "s"
-
-	id: Mapped[int] = mapped_column(primary_key=True)
