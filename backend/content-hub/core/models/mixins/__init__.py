a2795d73ad77c5b51f8b50042e99e2f3700ac99b
Artem <progerguru@gmail.com>
2025-04-22 10:01:49 +0300

    fix(models): add rating_mixin

--- backend/content-hub/core/models/mixins/__init__.py
+++ backend/content-hub/core/models/mixins/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "TimestampMixin",
     "UserIDMixin",
     "ContentMixin",
+    "RatingMixin",
 )
 
 from .id_mixin import IDMixin
 from .timestamp_mixin import TimestampMixin
 from .user_id_mixin import UserIDMixin
 from .content_mixin import ContentMixin
+from .rating_mixin import RatingMixin
