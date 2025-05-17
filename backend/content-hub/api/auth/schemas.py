4a7a25697c573c10c3d4b5e35cb15736c2e38243
milkazxc <petpetpetja1@gmail.com>
2025-05-17 17:26:59 +0300

    modified password validation

--- backend/content-hub/api/auth/schemas.py
+++ backend/content-hub/api/auth/schemas.py
@@ -1,16 +1,16 @@
-from pydantic import BaseModel, EmailStr, ConfigDict
+from pydantic import BaseModel, EmailStr, ConfigDict, constr
 
 
 class UserCreateInput(BaseModel):
     model_config = ConfigDict(strict=True)
     username: str
-    password: str
+    password: constr(min_length=8, max_length=30)
     email: EmailStr
 
 class UserLoginInput(BaseModel):
     model_config = ConfigDict(strict=True)
     username: str
-    password: str
+    password: constr(min_length=8, max_length=30)
 
 class Token(BaseModel):
     access_token: str
